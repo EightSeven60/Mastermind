a4efcf408b74c8b47395943fdda24ed0f82611e2
SMG5S <91834855+SMG5S@users.noreply.github.com>
2021-10-29 14:01:36 +0300

    Added Ball class

---
+++ src/Classes/Ball.java
@@ -0,0 +1,14 @@
+package Classes;
+
+public class Ball {
+    protected static final int COLOR_RED = 0;
+    protected static final int COLOR_BLUE = 1;
+    protected static final int COLOR_GREEN = 2;
+    protected static final int COLOR_YELLOW = 3;
+    protected static final int COLOR_TURQUOISE = 4;
+    protected static final int COLOR_PURPLE = 5;
+    public Ball()
+    {
+
+    }
+}
