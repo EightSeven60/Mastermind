fcb9f434e9529be3854aa1095d61cdfe2101ce50
SabauDavid <davidnvm3@gmail.com>
2021-10-30 19:30:10 +0300

    added a CounterClass to count the correctly guessed colours/positions and a CheckClass to check wheter or not the game is won and if the player has any turns left

---
+++ src/utilitymethods/CounterClass.java
@@ -0,0 +1,33 @@
+package utilitymethods;
+
+public class CounterClass {
+    public int positionCounter(int c1,int c2,int c3,int c4,int pc1,int pc2,int pc3, int pc4){//cx= colour from position x, pcx= picked colour from position x replace cx with colour [i][j] and pcx with CodeGenerator _getCodewx
+        int poscounter =0;
+        if(c1==pc1)
+            poscounter++;
+        if(c2==pc2)
+            poscounter++;
+        if(c3==pc3)
+            poscounter++;
+        if(c4==pc4)
+            poscounter++;
+        return poscounter;
+
+    }
+    public int colourCounter(int c1,int c2,int c3,int c4,int pc1,int pc2,int pc3,int pc4 ){ //can call colourCounter with the same parameters as positionCounter
+        int clrcounter=0;
+        if(c1==pc1||c1==pc2||c1==pc3||c1==pc4)
+            clrcounter++;
+        if(c2==pc1||c2==pc2||c2==pc3||c2==pc4)
+            clrcounter++;
+        if(c3==pc1||c3==pc2||c3==pc3||c3==pc4)
+            clrcounter++;
+        if(c4==pc1||c4==pc2||c4==pc3||c4==pc4)
+            clrcounter++;
+        return clrcounter;
+    }
+
+
+
+
+}
